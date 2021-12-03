ec6ff61f4e5ba825dff8bc963e40fb682af38f2a
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-03 13:16:36 -0600

    added new route for the alpha blog

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -5,4 +5,16 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def index
+        @allArticles = Article.all
+    end
+    
+    def new
+
+    end
+
+    def create
+        render plain: params[:article]
+    end
+
 end
