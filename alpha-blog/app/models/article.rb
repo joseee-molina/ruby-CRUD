4605c8b9820d66ee482aa06e4f219210ad976685
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-06 10:37:24 -0600

    added new min max for articles

--- alpha-blog/app/models/article.rb
+++ alpha-blog/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
     validates :title, presence: true, length: {minimum:6,maximum:100}
-    validates :description, presence: true
+    validates :description, presence: true, length: {minimum:5,maximum:500}
     
 end
