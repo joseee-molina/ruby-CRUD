d8be93449d92f2b3948dea7287cc4319b75fa426
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-06 12:49:51 -0600

    now we can edit specific articles

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -11,7 +11,12 @@ class ArticlesController < ApplicationController
     end
     
     def new
+      #the first time that we load the page, the new method gets called  
+        @newArticle = Article.new
+    end
 
+    def edit
+        @articleToEdit = Article.find(params[:id])
     end
 
     def create
@@ -19,8 +24,27 @@ class ArticlesController < ApplicationController
         #this render is going to show the params
         #in a blank page
         @newArticle = Article.new(params.require(:article).permit(:title,:description))
-        @newArticle.save
-        redirect_to article_path(@newArticle)
+        #filling new article with the fields of new
+        if @newArticle.save
+            flash[:notice] = "Article was created succesfully!!"
+            redirect_to article_path(@newArticle)
+        else
+            render 'new'
+            #this is calling new again
+        end
+    end
+
+    def update
+        @articleToEdit = Article.find(params[:id])
+        if @articleToEdit.update(params.require(:article).permit(:title, :description))
+                #filling edited article with the fields of new
+
+            flash[:notice] = "Article was edited succesfully!!"
+            redirect_to article_path(@articleToEdit)
+        else
+            render 'edit'
+            #this is calling new again
+        end
     end
 
 end
