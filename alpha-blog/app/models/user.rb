681b2e4a9ec6683040a6297ec9ee2d8be92ce73c
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-10 10:41:10 -0600

    added valdiations

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
 
+    validates :username, uniqueness: true, 
+                        presence: true, 
+                        length: {minimum: 3, maximum: 25}
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, uniqueness: {case_sensitive: false}, 
+                        presence: true, 
+                        length: {minimum: 3, maximum: 105},
+                        format: { with: VALID_EMAIL_REGEX }
+
+
 end
