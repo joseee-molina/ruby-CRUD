a1e24970ecfccf644b739ef37dcc4523b33193d9
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-15 10:14:32 -0600

    adding signup

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -6,5 +6,18 @@ class UsersController < ApplicationController
 
     def create
         #byebug
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Alpha Blog. Signed up succesfully"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
     end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
