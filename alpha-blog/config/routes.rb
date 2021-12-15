30d381ba59f8171904c80b1d2f990ac3f4f2b3d7
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-15 10:34:38 -0600

    can edit users, added templates and actions

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
   
-  resources :articles#, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles, :users#, only: [:show, :index, :new, :create, :edit, :update]
   
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
