7fe21d918d98f286e3260216175ed4be2fbae656
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-22 10:39:51 -0600

    adding validations and usind TDD

--- alpha-blog/test/models/category_test.rb
+++ alpha-blog/test/models/category_test.rb
@@ -1,9 +1,39 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-    "category should be valid"
-    test "category should be valid" do
+    #The setup method will create a category object available to
+    #all methods in the class, so that we write less code
+    def setup
         @category = Category.new(name: "Sports")
+    end
+
+    #"category should be valid"
+    test "category should be valid" do
+        #already declared in setup
         assert @category.valid?
     end
+
+    test "name should be present" do
+        #changing name from setup
+        @category.name= " "
+        assert_not(@category.valid?)
+    end
+
+    test "name should be unique" do
+        @category.save
+        @category2 = Category.new(name: "Sports")
+        assert_not(@category2.valid?)
+    end
+
+    test "name should not be too long" do
+        @category.name = 'a' * 26
+        assert_not @category.valid?
+    end
+
+    test "name should not be too short" do
+        @category.name = "aa"
+        assert_not @category.valid?
+    end
+
+
 end
