8c5acf8646f3f3e5ab428d213464d9fe59eb5eb7
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2021-12-22 11:45:53 -0600

    adding test for invaid category, and working fine

---
+++ alpha-blog/test/integration/create_category_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "get new category form and create category" do
+    get "/categories/new"
+    assert_response :success
+    
+    assert_difference 'Category.count',1 do
+      post categories_path, params: {category: {name: "Sports" } }
+      assert_response :redirect
+
+    end
+    follow_redirect!
+    #we want to follow the redirect and see where it takes us
+    assert_response :success
+    assert_match "Sports", response.body
+    #checking if Sports in in the body of the html
+  end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: " " } }
+    end
+    assert_match "errors", response.body
+    #it is looking for the word errors in the html page
+    assert_select 'div.alert'
+    #checking for this div in the html page
+    assert_select 'h4.alert-heading'
+    
+  end
+
+
+end
