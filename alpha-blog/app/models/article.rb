02bd852da44dcc32323ce1bd422611bec41c157c
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2022-01-05 16:07:47 -0600

    documenting code

--- alpha-blog/app/models/article.rb
+++ alpha-blog/app/models/article.rb
@@ -5,6 +5,6 @@ class Article < ApplicationRecord
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: {minimum:6,maximum:100}
     validates :description, presence: true, length: {minimum:5,maximum:500}
-    #important validations here ...
+    #important validations here!!
     
 end
